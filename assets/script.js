2c83d9b66d5092ee5c3f51eb76cb9b969bc5bfff
alexisbenavidez99 <callmealexis49@gmail.com>
2022-11-02 18:35:43 -0500

    added for loop for answer buttons and questions to js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,24 @@
 var scores = document.querySelector(".scores");
-var timer = document.querySelector(".timer");
+var timer = document.querySelector("#timer");
 var mainPage = document.querySelector(".quiz-intro");
 var startButton = document.querySelector(".start");
-var question = document.querySelector(".question-page");
+var question = document.querySelector("#question");
+var answerButtons = document.querySelectorAll(".answer");
 
 var secondsLeft = 60;
+var questions = ["How are you?", "What are you doing?"];
+var questionIndex = 0;
 
+var answerKey = {
+    0: {
+        possibleAnswers: {
+            'yes': true,
+            'no': false,
+            'mayne': false,
+            'check back with me later': false
+        }
+    }
+}
 function setTime() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
@@ -14,16 +27,25 @@ function setTime() {
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
         }
+
+        question.textContent = questions[questionIndex];
+        
+        const currentPossibleAnswers = Object.keys(answerKey[0])
+        console.log(currentPossibleAnswers)
+        
+        for(var i = 0; i < answerButtons.length; i++) {
+            answerButtons[i].textContent = currentPossibleAnswers[i]
+        }
         // if statement to deduct time if question is answered wrong
     }, 1000);
 }
 
-function question1 () {
-    
+for (var i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].addEventListener("click", function () {
+        questionIndex++;
+    })
 }
-
 
 startButton.addEventListener("click", () => {
     setTime();
-    question1();
 });
