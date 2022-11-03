bceb726249680775a35d86ce0074c676016c1745
alexisbenavidez99 <callmealexis49@gmail.com>
2022-11-03 18:22:23 -0500

    added questions and possible answers to js

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,48 @@ var question = document.querySelector("#question");
 var answerButtons = document.querySelectorAll(".answer");
 
 var secondsLeft = 60;
-var questions = ["How are you?", "What are you doing?"];
+var questions = ["Commonly used data types DO NOT include:", "The condition in an if / else statement is enclosed with ______.", "Arrays in JavaScript can be used to store ______.", "String values must be enclosed within ______ when being assigned to variables.", "A very useful tool used during development and debugging for printing content to the debugger is:"];
 var questionIndex = 0;
 
 var answerKey = {
     0: {
         possibleAnswers: {
-            'yes': true,
-            'no': false,
-            'mayne': false,
-            'check back with me later': false
+            'strings': false,
+            'booleans': false,
+            'alerts': true,
+            'numbers': false
+        }
+    },
+    1: {
+        possibleAnswers: {
+            'quotes': false,
+            'curly brackets': false,
+            'parentheses': true,
+            'square brackets': false
+        }
+    },
+    2: {
+        possibleAnswers: {
+            'numbers and strings': false,
+            'other arrays': false,
+            'booleans': false,
+            'all of the above': true,
+        }
+    },
+    3: {
+        possibleAnswers: {
+            'commas': false,
+            'curly brackets': false,
+            'quotes': true,
+            'parentheses': false
+        }
+    },
+    4: {
+        possibleAnswers: {
+            'JavaScript': false,
+            'Terminal/Bash': false,
+            'For loops': false,
+            'console.log': true
         }
     }
 }
